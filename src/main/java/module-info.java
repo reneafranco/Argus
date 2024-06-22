93f386b6cdaed4a21f21d5924c179126115c752b
Rene Franco <reneadonayfrancoperez@gmail.com>
2024-06-22 16:19:37 -0400

    grafical interface bar

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.example.argus {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens com.example.argus to javafx.fxml;
     exports com.example.argus;
 }
